e843f335f9571321ab2ce98d00a876692e41610f
Md Naeem <135458313+mdnaeemhossain@users.noreply.github.com>
2023-10-11 23:16:22 +0600

    Update index.php

--- index.php
+++ index.php
@@ -35,3 +35,16 @@ define( 'WP_MEMORY_LIMIT' , '512M' );
 		});
 	});
 </script>
+//Add this code in functions.php file
+// Redirect 404 not found page to home page
+if( !function_exists('redirect_404_to_homepage') ){
+
+    add_action( 'template_redirect', 'redirect_404_to_homepage' );
+
+    function redirect_404_to_homepage(){
+       if(is_404()):
+            wp_safe_redirect( home_url('/') );
+            exit;
+        endif;
+    }
+}
