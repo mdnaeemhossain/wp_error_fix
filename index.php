d3e6d7a301b2818f37499fd217c099451c4e3045
Md Naeem <135458313+mdnaeemhossain@users.noreply.github.com>
2023-10-22 18:03:37 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ define( 'WP_MAX_MEMORY_LIMIT' , '512M' );
 define( 'WP_MEMORY_LIMIT' , '512M' );
 
 {
+//Deprecated: rtrim(): Passing null to parameter #1 ($string) of type string is deprecated in G:\laragon\www\bdebest-wcl\wp-includes\formatting.php on line 2809
+return rtrim( $value ?? "", '/\\' );    //return rtrim( $value, '/\\' );
 Warning!!! Deprecated: strpos(): Passing null to parameter #1 in php 8.1 SOLVED
 //Deprecated: strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated in G:\laragon\www\bdebest-wcl\wp-includes\functions.php on line 7053
 
